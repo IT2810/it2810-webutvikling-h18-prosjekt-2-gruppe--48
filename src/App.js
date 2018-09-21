e1300054bbbedb5a9887fe2afb3370941b93bba4
PhilipPuente <philippuente@hotmail.com>
2018-09-21 17:36:26 +0200

    Connected tab and checkbox functionality to other components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import Tabs from './Tabs.js';
 import CategoryBox from './Category.js'
 
 const cachios = require("cachios");
 
+var updateMap = {};
 var categories = ["animals", "city", "nature"];
-var currentCategory = "animals";
-var currentIndex = "1";
+var currentCategory = { "text": "animals", 
+                        "audio": "animals", 
+                        "images": "animals"};
+var currentIndex = 1;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { active: 'tab1' };
+    this.callback = this.callback.bind(this);
   }
   render() {
     const content = {
@@ -31,26 +34,36 @@ class App extends Component {
             <div id="tab3" className="tab" key="tab3">Tab component 3</div>
             <div id="tab4" className="tab" key="tab4">Tab component 4</div>
           </Tabs>
-          <div id="image">{content[this.state.active]}</div>
-          <div id="text">Text component goes here</div>
-          <div id="audio">Audio component goes here</div>
-          <div id="image"><SVGComponent index="2" /></div>
+          <div id="image"><SVGComponent /></div>
           <div id="text"><PoemComponent /></div>
           <div id="audio"><AudioComponent /></div>
           <div id="category-image"><CategoryBox boxName="images" categoryNames={categories} callback={this.callback}
           /></div>
-           <div id="category-audio"><CategoryBox boxName="sounds" categoryNames={categories} callback={this.callback}
+          <div id="category-audio"><CategoryBox boxName="audio" categoryNames={categories} callback={this.callback}
           /></div>
-           <div id="category-text"><CategoryBox boxName="text" categoryNames={categories} callback={this.callback}
+          <div id="category-text"><CategoryBox boxName="text" categoryNames={categories} callback={this.callback}
           /></div>
         </div>
       </div>
     );
   }
+  callback(categories, boxName) {
+    for (var [key, value] of categories.entries()) {
+      if (value) {
+        currentCategory[boxName] = key;
+      }
+      console.log(boxName, key, value);
+    }
+    switch (boxName) {
+      case "audio": updateMap["audio"].setSource(currentCategory["audio"], currentIndex); break;
+      case "text": getResource(updateMap["text"]); break;
+      case "images": getResource(updateMap["images"]); break;
+    }
+  }
 }
 
 function getResource(component) {
-  cachios.get(`${component.prefix}${currentCategory}${currentIndex}${component.suffix}`)
+  cachios.get(`${component.prefix}/${currentCategory[component.prefix]}${currentIndex}${component.suffix}`)
     .then(response => {
       component.setState({
         isLoaded: true,
@@ -81,8 +94,9 @@ class AJAXComponent extends Component {
 class SVGComponent extends AJAXComponent {
   constructor(props) {
     super(props);
-    this.prefix = "images/";
+    this.prefix = "images";
     this.suffix = ".svg";
+    updateMap["images"] = this;
   }
 
   render() {
@@ -101,8 +115,9 @@ class SVGComponent extends AJAXComponent {
 class PoemComponent extends AJAXComponent {
   constructor(props) {
     super(props);
-    this.prefix = "text/";
-    this.suffix = ".json";
+    this.prefix = "text";
+    this.suffix = ".json";    
+    updateMap["text"] = this;
   }
 
   render() {
@@ -124,11 +139,45 @@ class PoemComponent extends AJAXComponent {
 }
 
 class AudioComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.setSource = this.setSource.bind(this);
+    this.source = `audio/${currentCategory["audio"]}${currentIndex}.mp3`;
+    updateMap["audio"] = this;
+  }
+  setSource(category, index) {
+    console.log(category, index);
+    this.source = `audio/${category}${index}.mp3`;
+    this.forceUpdate();
+  }
   render() {
     return (
-      <audio controls src={`sounds/${currentCategory}${currentIndex}.mp3`} type="audio/mpeg">
+      <audio id="audio-player" controls src={this.source} type="audio/mpeg">
         Audio player not supported.
       </audio>
+    );
+  }
+}
+
+class Tabs extends Component {
+  render() {
+    return (
+      <div className="Tabs">
+        {React.Children.map(this.props.children, (child, i) => {
+          return (
+            <div className="Tabs__Tab" onClick={() => {
+              currentIndex = i + 1;
+              getResource(updateMap["images"]);
+              getResource(updateMap["text"]);
+              updateMap["audio"].setSource(currentCategory["audio"], currentIndex);
+              this.props.onChange(child.key);
+            }}>
+              {child}
+            </div>
+          );
+        })}
+      </div>
+
     );
   }
 }
